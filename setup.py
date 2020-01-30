e94fe233c07f1e897b5f92350546140bc41d7633
D. Duckie Rodriguez <duckie.68@gmail.com>
2020-01-30 09:53:52 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dproj',
+        version='0.1',
+        description='Duckies project manager',
+        url='https://github.com/duckie68/proj',
+        author='D. Duckie Rodriguez',
+        author_email='duckie.68@gmail.com',
+        license='MIT',
+        packages=['dproj'],
+        zip_safe=False)
